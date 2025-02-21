421c533f3ad0370545a57004d6c5d52ca35da5e0
muhammadhafiz <khodjayev.muhammad@gmail.com>
2025-02-21 20:55:14 +0500

    initial commit

---
+++ problems/Program.cs
@@ -0,0 +1,138 @@
+﻿// 1. Ikkinchi eng katta sonni topish
+
+// Kirish (Input):
+
+// 10, 25, 30, 15, 40, 50
+
+// Chiqish (Output):
+
+// Eng katta son: 50  
+// Ikkinchi eng katta son: 40
+
+
+// ---
+
+// 2. Eng ko‘p uchragan elementni topish
+
+// Kirish (Input):
+
+// 4, 5, 6, 4, 7, 4, 5, 6
+
+// Chiqish (Output):
+
+// Eng ko‘p uchragan element: 4 (3 marta)
+
+
+// ---
+
+// 3. Ro‘yxatni o‘ngga surish
+
+// Kirish (Input):
+
+// 1, 2, 3, 4, 5
+
+// Chiqish (Output):
+
+// 5, 1, 2, 3, 4
+
+
+// ---
+
+// 4. Ro‘yxatni N marta aylantirish
+
+// Kirish (Input):
+
+// Ro‘yxat: 10, 20, 30, 40, 50  
+// N: 2
+
+// Chiqish (Output):
+
+// 30, 40, 50, 10, 20
+
+
+// ---
+
+// 5. Ikkita ro‘yxatni aralashtirib qo‘shish
+
+// Kirish (Input):
+
+// Ro‘yxat 1: 1, 3, 5  
+// Ro‘yxat 2: 2, 4, 6
+
+// Chiqish (Output):
+
+// 1, 2, 3, 4, 5, 6
+
+
+// ---
+
+// 6. Ro‘yxatning bir qismini teskari qilish
+
+// Kirish (Input):
+
+// Ro‘yxat: 1, 2, 3, 4, 5, 6, 7, 8  
+// Boshlang‘ich indeks: 2  
+// Oxirgi indeks: 6
+
+// Chiqish (Output):
+
+// 1, 2, 6, 5, 4, 3, 7, 8
+
+
+// ---
+
+// 7. Raqamlarni raqamlar yig‘indisiga qarab saralash
+
+// Kirish (Input):
+
+// Ro‘yxat: 12, 25, 9, 38, 41
+
+// Chiqish (Output):
+
+// 9 (9)  
+// 12 (1+2=3)  
+// 41 (4+1=5)  
+// 25 (2+5=7)  
+// 38 (3+8=11)
+
+
+// ---
+
+// 8. Yo‘qolgan sonni topish
+
+// Kirish (Input):
+
+// 1, 2, 3, 5, 6
+
+// Chiqish (Output):
+
+// Yo‘qolgan son: 4
+
+
+// ---
+
+// 9. Ro‘yxatni toq va juft sonlarga ajratish
+
+// Kirish (Input):
+
+// 10, 15, 20, 25, 30, 35
+
+// Chiqish (Output):
+
+// Juft sonlar: 10, 20, 30  
+// Toq sonlar: 15, 25, 35
+
+
+// ---
+
+// 10. So‘zlarni bosh harfiga qarab guruhlash
+
+// Kirish (Input):
+
+// "apple", "banana", "cherry", "apricot", "blueberry"
+
+// Chiqish (Output):
+
+// A: apple, apricot  
+// B: banana, blueberry  
+// C: cherry
