0aff600012a8cd84b0b6f91f0866f5c6d61158ff
Firdavs <firdavsmukhsimov@gmail.com>
2025-02-23 13:47:02 +0500

    5 masala

--- problems/Program.cs
+++ problems/Program.cs
@@ -1,138 +1,88 @@
-﻿// 1. Ikkinchi eng katta sonni topish
+﻿class Program
+{
+    public static void Main()
+    {
+        //1
+        // class Program
+    // {
+    //     public static void Main()
+    //     {
 
-// Kirish (Input):
+    //         int[] arr = [10 , 25, 30, 15, 40, 50];
 
-// 10, 25, 30, 15, 40, 50
+    //         int temp = 0;
 
-// Chiqish (Output):
+    //         int next = 0;
 
-// Eng katta son: 50  
-// Ikkinchi eng katta son: 40
+    //         for(int i = 0; i < arr.Length; i++)
+    //         {
+    //             if(temp < arr[i])
+    //             {
+    //                 next = temp;
 
+    //                 temp = arr[i];
+    //             }
+    //         }
 
-// ---
+    //         Console.WriteLine(next);
+    //     }
+    // }
 
-// 2. Eng ko‘p uchragan elementni topish
 
-// Kirish (Input):
 
-// 4, 5, 6, 4, 7, 4, 5, 6
 
-// Chiqish (Output):
 
-// Eng ko‘p uchragan element: 4 (3 marta)
+    //3
+    // int[] arr = [1, 2, 3, 4, 5];
 
+    // int temp = 5;
 
-// ---
+    // for(int i = 4; i > 0; )
+    // {
+    //     arr[i] = arr[i-1];
 
-// 3. Ro‘yxatni o‘ngga surish
+    //     i--;
+    // }
+    
+    // arr[0] = temp;
 
-// Kirish (Input):
+    // for(int i = 0; i < 5; i++)
+    // {
+    //     Console.WriteLine(arr[i]);
+    // }
 
-// 1, 2, 3, 4, 5
 
-// Chiqish (Output):
 
-// 5, 1, 2, 3, 4
+    //5
 
+    // int[] arr1 = [1, 3, 5];
 
-// ---
+    // int[] arr2 = [2, 4, 6];
 
-// 4. Ro‘yxatni N marta aylantirish
+    // for(int i = 0; i <= 3; i++)
+    // {
+    //     Console.Write(arr1[i] + ", " + arr2[i] + ", ");
+    // }
+    // Console.WriteLine();
 
-// Kirish (Input):
+    // }
 
-// Ro‘yxat: 10, 20, 30, 40, 50  
-// N: 2
+    //7
 
-// Chiqish (Output):
+    int[] arr = [12, 25, 9, 38, 41];
 
-// 30, 40, 50, 10, 20
+    for(int i = 0; i <= arr.Length; i++)
+    {
+        if(arr[i] > 9)
+        {
+            Console.WriteLine($"{arr[i]}-({arr[i] / 10}+{arr[i] % 10}={arr[i] / 10 + arr[i] % 10})");
+        }
+        else 
+        {
+            Console.WriteLine(arr[i] + $"({arr[i]})");
+        }
+    }
 
 
-// ---
-
-// 5. Ikkita ro‘yxatni aralashtirib qo‘shish
-
-// Kirish (Input):
-
-// Ro‘yxat 1: 1, 3, 5  
-// Ro‘yxat 2: 2, 4, 6
-
-// Chiqish (Output):
-
-// 1, 2, 3, 4, 5, 6
-
-
-// ---
-
-// 6. Ro‘yxatning bir qismini teskari qilish
-
-// Kirish (Input):
-
-// Ro‘yxat: 1, 2, 3, 4, 5, 6, 7, 8  
-// Boshlang‘ich indeks: 2  
-// Oxirgi indeks: 6
-
-// Chiqish (Output):
-
-// 1, 2, 6, 5, 4, 3, 7, 8
-
-
-// ---
-
-// 7. Raqamlarni raqamlar yig‘indisiga qarab saralash
-
-// Kirish (Input):
-
-// Ro‘yxat: 12, 25, 9, 38, 41
-
-// Chiqish (Output):
-
-// 9 (9)  
-// 12 (1+2=3)  
-// 41 (4+1=5)  
-// 25 (2+5=7)  
-// 38 (3+8=11)
-
-
-// ---
-
-// 8. Yo‘qolgan sonni topish
-
-// Kirish (Input):
-
-// 1, 2, 3, 5, 6
-
-// Chiqish (Output):
-
-// Yo‘qolgan son: 4
-
-
-// ---
-
-// 9. Ro‘yxatni toq va juft sonlarga ajratish
-
-// Kirish (Input):
-
-// 10, 15, 20, 25, 30, 35
-
-// Chiqish (Output):
-
-// Juft sonlar: 10, 20, 30  
-// Toq sonlar: 15, 25, 35
-
-
-// ---
-
-// 10. So‘zlarni bosh harfiga qarab guruhlash
-
-// Kirish (Input):
-
-// "apple", "banana", "cherry", "apricot", "blueberry"
-
-// Chiqish (Output):
-
-// A: apple, apricot  
-// B: banana, blueberry  
-// C: cherry
+}
+}
