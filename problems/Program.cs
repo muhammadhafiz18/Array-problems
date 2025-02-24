64e73365cedcadfcbb350ff6c12b9badde40c3ed
Firdavs <firdavsmukhsimov@gmail.com>
2025-02-24 18:38:37 +0500

    Firdavs

--- problems/Program.cs
+++ problems/Program.cs
@@ -1,4 +1,7 @@
-﻿class Program
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+
+class Program
 {
     public static void Main()
     {
@@ -28,9 +31,35 @@
     //     }
     // }
 
+    
 
+    //2;
 
+    // int[] arr = [4, 5, 6, 4, 7, 4, 5, 6];
+    // int c = arr[0];
+    // int max = 0;
+    
+    // foreach (int num in arr)
+    // {
+    //     int temp = 0;
 
+    //     foreach(int tem in arr)
+    //     {
+    //         if(tem == num)
+    //         {
+    //             temp++;
+    //         }
+    //     }
+
+    //     if(temp > max)
+    //     {
+    //         max = temp;
+
+    //         c = num;
+
+    //     }
+    // }
+    // Console.WriteLine(c);
 
     //3
     // int[] arr = [1, 2, 3, 4, 5];
@@ -51,8 +80,6 @@
     //     Console.WriteLine(arr[i]);
     // }
 
-
-
     //5
 
     // int[] arr1 = [1, 3, 5];
@@ -69,20 +96,34 @@
 
     //7
 
-    int[] arr = [12, 25, 9, 38, 41];
+    // List<int> arr = [12, 25, 9, 38, 41];
 
-    for(int i = 0; i <= arr.Length; i++)
-    {
-        if(arr[i] > 9)
-        {
-            Console.WriteLine($"{arr[i]}-({arr[i] / 10}+{arr[i] % 10}={arr[i] / 10 + arr[i] % 10})");
-        }
-        else 
-        {
-            Console.WriteLine(arr[i] + $"({arr[i]})");
-        }
+    // arr.Sort();
+
+    // for(int i = 0; i <= arr.Count; i++)
+    // {
+    //     if(arr[i] > 9)
+    //     {
+    //         Console.WriteLine($"{arr[i]}-({arr[i] / 10}+{arr[i] % 10}={arr[i] / 10 + arr[i] % 10})");
+    //     }
+    //     else 
+    //     {
+    //         Console.WriteLine(arr[i] + $"({arr[i]})");
+    //     }
+    // }
+
+//     //6
+
+    List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6, 7, 8};
+        int N = 2;
+        int M = 6;
+        
+        var reversedNumbers = numbers.GetRange(N, M - N);
+        numbers.RemoveRange(N, M - N);
+        reversedNumbers.Reverse();
+        numbers.InsertRange(N, reversedNumbers);
+
+        Console.WriteLine(string.Join(", ", numbers));
+
     }
-
-
-}
-}
+}     
