68a0367322cce35e2417dfc734a6e362fa28ca6d
umida <riksiyevaumida@gmail.com>
2025-02-24 17:12:31 +0500

    almost alll problems solved

--- problems/Program.cs
+++ problems/Program.cs
@@ -1,138 +1,116 @@
-﻿// 1. Ikkinchi eng katta sonni topish
+﻿//1
 
-// Kirish (Input):
+// int[] sonlar = {10, 25, 30, 15, 40, 50};
 
-// 10, 25, 30, 15, 40, 50
+//         int max = sonlar[0];
+//         int Max = sonlar[0];
 
-// Chiqish (Output):
+//         for(int i = 1; i < sonlar.Length; i++)
+//         {
+//             if(sonlar[i] > max)
+//             {
+//                 Max = max;
+//                 max = sonlar[i];
+//             }
+//             else if(sonlar[i] > Max && sonlar[i] != Max)
+//             {
+//                 Max = sonlar[i];
+//             }
+//         }
 
-// Eng katta son: 50  
-// Ikkinchi eng katta son: 40
+//     Console.WriteLine("Eng katta son: " + max);
+//     Console.WriteLine("Ikkinchi eng katta son: " + Max);
 
+//2
 
-// ---
+// int[] sonlar = {4, 5, 6, 4, 7, 4, 5, 6};
 
-// 2. Eng ko‘p uchragan elementni topish
+// int son = sonlar[0];
+// int nechamarta = 1;
 
-// Kirish (Input):
+// for(int i = 0;i < sonlar.Length;i++)
+// {
+//     for(int m = 0;m < sonlar.Length;m++)
+//     {
+//         if(sonlar[i] == sonlar[m])
+//         {
+//             nechamarta++;
+//         }
+//     }
+// }
+// Console.WriteLine($"Eng ko'p uchragan son: {son} ({nechamarta})");
 
-// 4, 5, 6, 4, 7, 4, 5, 6
 
-// Chiqish (Output):
+//3
 
-// Eng ko‘p uchragan element: 4 (3 marta)
+//  int[] sonlar = {1, 2, 3, 4, 5};
+//  int uzunlik = sonlar.Length;
+//  int Oxirgi = sonlar[uzunlik - 1]; 
 
+//  for(int i = uzunlik -1; i > 0; i--)  
+//  {
+//      sonlar[i] = sonlar[i - 1]; 
+//  }
 
-// ---
+//  sonlar[0] = Oxirgi;
 
-// 3. Ro‘yxatni o‘ngga surish
+//  System.Console.WriteLine(string.Join(", ",sonlar));
 
-// Kirish (Input):
 
-// 1, 2, 3, 4, 5
+//5
 
-// Chiqish (Output):
+// int[] list1 = { 1, 3, 5 };
+// int[] list2 = { 2, 4, 6 };
+// int[] natija = new int[list1.Length + list2.Length];
 
-// 5, 1, 2, 3, 4
+// int index = 0;
+// for (int i = 0; i < list1.Length; i++)
+// {
+//     natija[index++] = list1[i];
+//     natija[index++] = list2[i];
+// }
 
+// Console.WriteLine(string.Join(", ", natija));
 
-// ---
+//8
 
-// 4. Ro‘yxatni N marta aylantirish
+// int[] sonlar = {1,2,3,5,6};
+// int num1 = sonlar.Length + 1; //
+// int num2 = 0;
+// int num3 = 0;
+        
+// for(int i = num1; i >= 0; i--)//
+// {
+//     num2 += i; //num2 = 21
+// }
+// for(int i = 0; i < sonlar.Length; i++) //
+// {
+//     num3 += sonlar[i]; //
+// }
 
-// Kirish (Input):
+// int num4 = num2 - num3;
 
-// Ro‘yxat: 10, 20, 30, 40, 50  
-// N: 2
+// Console.WriteLine($"Yo'qolgan son: {num4}");
 
-// Chiqish (Output):
+//9
 
-// 30, 40, 50, 10, 20
+// int[] numbers = {10, 15, 20, 25, 30, 35};
 
+// List<int> juft = new List<int>(); 
+// List<int> toq = new List<int>();
 
-// ---
+// foreach (int num in numbers) 
+// {
+//     if(num % 2 == 0)
+//     {
+//         juft.Add(num);
+//     }
+//     else
+//     {
+//         toq.Add(num);
+//     }
+// }
+
+// Console.WriteLine("Juft sonlar: " + string.Join(", ", juft));
+// Console.WriteLine("Toq sonlar: " + string.Join(", ", toq));
 
-// 5. Ikkita ro‘yxatni aralashtirib qo‘shish
-
-// Kirish (Input):
-
-// Ro‘yxat 1: 1, 3, 5  
-// Ro‘yxat 2: 2, 4, 6
-
-// Chiqish (Output):
-
-// 1, 2, 3, 4, 5, 6
-
-
-// ---
-
-// 6. Ro‘yxatning bir qismini teskari qilish
-
-// Kirish (Input):
-
-// Ro‘yxat: 1, 2, 3, 4, 5, 6, 7, 8  
-// Boshlang‘ich indeks: 2  
-// Oxirgi indeks: 6
-
-// Chiqish (Output):
-
-// 1, 2, 6, 5, 4, 3, 7, 8
-
-
-// ---
-
-// 7. Raqamlarni raqamlar yig‘indisiga qarab saralash
-
-// Kirish (Input):
-
-// Ro‘yxat: 12, 25, 9, 38, 41
-
-// Chiqish (Output):
-
-// 9 (9)  
-// 12 (1+2=3)  
-// 41 (4+1=5)  
-// 25 (2+5=7)  
-// 38 (3+8=11)
-
-
-// ---
-
-// 8. Yo‘qolgan sonni topish
-
-// Kirish (Input):
-
-// 1, 2, 3, 5, 6
-
-// Chiqish (Output):
-
-// Yo‘qolgan son: 4
-
-
-// ---
-
-// 9. Ro‘yxatni toq va juft sonlarga ajratish
-
-// Kirish (Input):
-
-// 10, 15, 20, 25, 30, 35
-
-// Chiqish (Output):
-
-// Juft sonlar: 10, 20, 30  
-// Toq sonlar: 15, 25, 35
-
-
-// ---
-
-// 10. So‘zlarni bosh harfiga qarab guruhlash
-
-// Kirish (Input):
-
-// "apple", "banana", "cherry", "apricot", "blueberry"
-
-// Chiqish (Output):
-
-// A: apple, apricot  
-// B: banana, blueberry  
-// C: cherry
